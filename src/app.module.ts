995d5438012e16b1243cf07b8a2cc35532de67b9
JoySarkarBD <developer.joysarkar@gmail.com>
2024-06-01 11:00:33 +0600

    Simple Authentication Middleware Example

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,3 +9,31 @@ import { UsersModule } from './users/users.module';
   providers: [AppService],
 })
 export class AppModule {}
+
+/* The configure method is used to apply middleware to routes. */
+// export class AppModule {
+//   configure(consumer: MiddlewareConsumer) {
+//     consumer.apply(AuthMiddleware).forRoutes(UsersController); // Apply the AuthMiddleware to all routes defined in the UsersController.
+//   }
+// }
+
+// This is the way to add the middleware to the whole module form the root app module, we can simply use this inside of out desire module like in our case it's user module
+// export class AppModule {
+//   configure(consumer: MiddlewareConsumer) {
+//     consumer.apply(AuthMiddleware).forRoutes(
+//       { path: 'users', method: RequestMethod.ALL }, // Apply to root path
+//       { path: 'users/*', method: RequestMethod.ALL }, // Apply to all sub routes in UsersController
+//     );
+//   }
+// }
+
+/* This is the way to add the middleware to the specific route form the root app module, we can simply use this inside of out desire module like in our case it's user module */
+// export class AppModule {
+//   configure(consumer: MiddlewareConsumer) {
+//     consumer.apply(AuthMiddleware).forRoutes(
+//       { path: 'users/get-all-users', method: RequestMethod.GET }, // Apply to GET /users/get-all-users
+//       { path: 'users/create-user', method: RequestMethod.POST }, // Apply to POST /users/create-user
+//       { path: 'users/:id', method: RequestMethod.GET }, // Apply to GET /users/:id
+//     );
+//   }
+// }
