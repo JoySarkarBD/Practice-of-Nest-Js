7de4dac320826ba8157e4841a42ca766f5946db9
JoySarkarBD <developer.joysarkar@gmail.com>
2024-06-07 01:49:10 +0600

    Cors & Helmet Middleware Implementation

--- src/middleware/helmet.middleware.ts
+++ src/middleware/helmet.middleware.ts
@@ -4,6 +4,12 @@ import helmet from 'helmet';
 
 @Injectable()
 export class HelmetMiddleware implements NestMiddleware {
+  /**
+   * Middleware function to apply Helmet security headers to the HTTP response.
+   * @param req - The incoming HTTP request object.
+   * @param res - The HTTP response object.
+   * @param next - The next function to call in the middleware chain.
+   */
   use(req: Request, res: Response, next: NextFunction) {
     helmet()(req, res, next);
   }
