dd0b4214974b4d6ab2124cb3e311af9c834285bf
JoySarkarBD <developer.joysarkar@gmail.com>
2024-06-07 01:41:57 +0600

    Custom Response Interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductionExceptionFilter } from './exception-filter/custom-exception.filter';
+import { ResponseInterceptor } from './interceptors/response.interceptor';
 import { CookieParserMiddleware } from './middleware/cookie-parser.middleware';
 import { CorsMiddleware } from './middleware/cors.middleware';
 import { HelmetMiddleware } from './middleware/helmet.middleware';
@@ -19,12 +20,16 @@ import { UsersModule } from './users/users.module';
       provide: APP_FILTER,
       useClass: ProductionExceptionFilter,
     },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseInterceptor,
+    },
   ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     // Apply Morgan middleware to log HTTP requests.
-    // Apply Helmet middleware to enhance security by setting security-related HTTP response headers. The 'csp' middleware sets the Content-Security-Policy header to prevent XSS attacks, while 'hidePoweredBy' removes the X-Powered-By header.
+    // Apply Helmet middleware to enhance security by setting security-related HTTP response headers.
     // Apply CORS middleware to enable Cross-Origin Resource Sharing (CORS), allowing resources to be requested from another domain.
     // Apply cookie-parser middleware to parse cookies from incoming requests.
     // Apply rate-limit middleware to limit the rate of incoming requests from clients.
@@ -41,30 +46,33 @@ export class AppModule implements NestModule {
   }
 }
 
-/* The configure method is used to apply middleware to routes. */
-// export class AppModule {
-//   configure(consumer: MiddlewareConsumer) {
-//     consumer.apply(AuthMiddleware).forRoutes(UsersController); // Apply the AuthMiddleware to all routes defined in the UsersController.
-//   }
-// }
+// Commented out sections demonstrating different ways to apply middleware, kept for reference.
+/* 
+// The configure method is used to apply middleware to routes.
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(UsersController); // Apply the AuthMiddleware to all routes defined in the UsersController.
+  }
+}
 
-// This is the way to add the middleware to the whole module form the root app module, we can simply use this inside of out desire module like in our case it's user module
-// export class AppModule {
-//   configure(consumer: MiddlewareConsumer) {
-//     consumer.apply(AuthMiddleware).forRoutes(
-//       { path: 'users', method: RequestMethod.ALL }, // Apply to root path
-//       { path: 'users/*', method: RequestMethod.ALL }, // Apply to all sub routes in UsersController
-//     );
-//   }
-// }
+// This is the way to add the middleware to the whole module from the root app module. We can simply use this inside of our desired module, like in our case it's the user module.
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(
+      { path: 'users', method: RequestMethod.ALL }, // Apply to root path
+      { path: 'users/*', method: RequestMethod.ALL }, // Apply to all sub-routes in UsersController
+    );
+  }
+}
 
-/* This is the way to add the middleware to the specific route form the root app module, we can simply use this inside of out desire module like in our case it's user module */
-// export class AppModule {
-//   configure(consumer: MiddlewareConsumer) {
-//     consumer.apply(AuthMiddleware).forRoutes(
-//       { path: 'users/get-all-users', method: RequestMethod.GET }, // Apply to GET /users/get-all-users
-//       { path: 'users/create-user', method: RequestMethod.POST }, // Apply to POST /users/create-user
-//       { path: 'users/:id', method: RequestMethod.GET }, // Apply to GET /users/:id
-//     );
-//   }
-// }
+// This is the way to add the middleware to the specific route from the root app module. We can simply use this inside of our desired module, like in our case it's the user module.
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(
+      { path: 'users/get-all-users', method: RequestMethod.GET }, // Apply to GET /users/get-all-users
+      { path: 'users/create-user', method: RequestMethod.POST }, // Apply to POST /users/create-user
+      { path: 'users/:id', method: RequestMethod.GET }, // Apply to GET /users/:id
+    );
+  }
+}
+*/
